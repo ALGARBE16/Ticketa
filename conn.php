2fd90c2ca4b6247b27509b276652b6c7920825a2
Carniel <Carniel@DESKTOP-6R4CS5V>
2025-06-14 17:55:44 -0300

    cambios

--- conn.php
+++ conn.php
@@ -1,3 +1,8 @@
 <?php
-echo "<?php\n";
-// Database connection settings                         
+$conn = new PDO('mysql:host=localhost;dbname=soporte_tecnico', 'root', '');
+if (!$conn) {
+    die("Error de conexión: " . $conn->errorInfo());
+} else {
+    echo "Conexión exitosa a la base de datos.";
+}
+?>
